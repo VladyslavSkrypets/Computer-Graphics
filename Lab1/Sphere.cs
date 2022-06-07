510f7ecc5592595008841263741398110a1b4206
Vlad Skripets <vsk@jooble.com>
2022-06-07 23:32:37 +0300

    added basic sphere class

---
+++ Lab1/Sphere.cs
@@ -0,0 +1,13 @@
+namespace Lab1;
+
+public class Sphere
+{
+    public Sphere(float radius, Point center)
+    {
+        Radius = radius;
+        Center = center;
+    }
+    
+    public float Radius { get; set; }
+    public Point Center { get; set; } 
+}
