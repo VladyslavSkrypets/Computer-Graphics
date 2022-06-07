1928839432f2da689931a5a870d85fce8e0ae311
Vlad Skripets <vsk@jooble.com>
2022-06-07 23:30:59 +0300

    added new Point class

---
+++ Lab1/Point.cs
@@ -0,0 +1,25 @@
+namespace Lab1;
+
+public class Point
+{
+    public Point(float x = 0, float y = 0, float z = 0)
+    {
+        X = x;
+        Y = y;
+        Z = z;
+
+    }
+
+    public float X { get; set; }
+    public float Y { get; set; }
+    public float Z { get; set; }
+    
+    public void Print()
+    {
+        Console.WriteLine($"Point(x={X}, y={Y}, x={Z})");
+    }
+    
+    
+    
+    
+}
