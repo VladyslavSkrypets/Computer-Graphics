09a75873b16944b9b044b35484a9f016c12b6624
Anton <antonfiveis@gmail.com>
2022-06-10 00:06:18 +0300

    adding basic params to scene

--- Lab1/Scene.cs
+++ Lab1/Scene.cs
@@ -2,5 +2,18 @@ namespace Lab1;
 
 public class Scene
 {
-    
+    Scene(Point start, Area area, int width = 20, int height = 20, float cellSize = 2)
+    {
+        _Area = area;
+        _Start = start;
+        _Width = width;
+        _Height = height;
+        _CellSize = cellSize;
+    }
+
+    public float _CellSize { get; set; }
+    public Point _Start { get; set; }
+    public Area _Area { get; set; }
+    public int _Width { get; set; }
+    public int _Height { get; set; }
 }
